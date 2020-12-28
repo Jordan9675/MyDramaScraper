79b47ff176a5d255c8b526874b3bc0c9a51e989d
Jordan9675 <jordan.vuong96@gmail.com>
2020-12-28 11:02:06 +0100

    stopped apostrophes escaping

--- dramascraper/dramascraper/pipelines.py
+++ dramascraper/dramascraper/pipelines.py
@@ -47,23 +47,6 @@ class InsertItem:
 
         return item
 
-    def unescape_apostrophes(self, item):
-        """
-        Replace every single quote by double quotes to avoid any trouble
-        during the SQL insertion
-
-        Args:
-            item (dict): item returned by our Scrapy spider
-
-        Returns:
-            dict: Informations about a drama with escaped single quotes
-        """
-        for key in item:
-            if isinstance(item[key], str):
-                item[key] = item[key].replace("'", "''")
-
-        return item
-
     def process_item(self, item, spider):
         """
         Method that is performed on each item returned by our spider and which
@@ -81,7 +64,6 @@ class InsertItem:
                     "supportingroles, guestroles) VALUES (%s, %s, %s, %s, %s, " \
                     "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
             item = self.convert_to_json_string(item)
-            item = self.unescape_apostrophes(item)
             values = tuple(item.values())
             self.cursor.execute(query, values)
             self.db.commit()
